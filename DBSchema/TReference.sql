c350e91aa836cd3ef0b969024e09de69001dc26e
quangnle <mnest2409@yahoo.com>
2017-03-30 14:29:32 +0800

    commit

---
+++ DBSchema/TReference.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[TReference]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Initial] VARCHAR(8) NOT NULL, 
+    [FirstName] NVARCHAR(15) NOT NULL, 
+    [LastName] NVARCHAR(25) NOT NULL, 
+    [Relationship] NVARCHAR(20) NULL, 
+    [Email] NVARCHAR(20) NULL, 
+    [Phone] VARCHAR(15) NULL, 
+    [IdProfile] INT NOT NULL
+)
