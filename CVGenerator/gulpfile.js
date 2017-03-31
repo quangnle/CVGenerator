c9b133fd216b608fcf101a0b5d8b1a0bc5d63edd
Ryan Quoc Le <quoc.letranminh@gmail.com>
2017-03-31 19:01:01 +0800

    config gulpfile

---
+++ CVGenerator/gulpfile.js
@@ -0,0 +1,32 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var del = require('del');
+var concat = require('gulp-concat');
+
+var outputLocation = 'dist';
+
+gulp.task('clean', function () {
+    del.sync([outputLocation + '/**']);
+});
+
+gulp.task('vendor-scripts', function () {
+    var vendorSources = {
+        jquery: [
+                'node_modules/jquery/dist/jquery.min.js',
+                'node_modules/angular/angular.min.js',
+	            'node_modules/bootstrap/dist/js/bootstrap.min.js',
+                'node_modules/toastr/build/toastr.min.js'
+        ]
+    }
+
+    gulp.src(vendorSources.jquery)
+		.pipe(concat('jquery.bundle.min.js'))
+		.pipe(gulp.dest(outputLocation + '/scripts/'));
+});
+
+
+gulp.task('default', ['clean', 'vendor-scripts'], function () { });
