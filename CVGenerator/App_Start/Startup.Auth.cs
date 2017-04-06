bdbeaa1c7716305375343a51a5dad3ed9c19ad49
Ryan Quoc Le <quoc.letranminh@gmail.com>
2017-04-06 18:28:18 +0800

    update

---
+++ CVGenerator/App_Start/Startup.Auth.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CVGenerator
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "Cookies",
+                LoginPath = new PathString("/Account/Login"),
+                //Provider = new CookieAuthenticationProvider
+                //{
+                //    //OnApplyRedirect = ApplyRedirect
+                //},
+                //AuthenticationMode = AuthenticationMode.Active,
+                //CookieSecure = CookieSecureOption.Always
+            });
+        }
+    }
+}
---
+++ CVGenerator/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CVGenerator.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            AuthenticationManager.SignOut("Cookies");
+            //AuthenticationManager.SignOut(DefaultAuthenticationTypes.ExternalCookie);
+ 
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            AuthenticationManager.SignOut("Cookies");
+            //AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            //AuthenticationManager.SignOut(DefaultAuthenticationTypes.ExternalCookie);
+            return View();
+        }
+    }
+}
