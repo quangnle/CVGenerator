347c91aa59ad46245b5a95eb32a313dd1b68c008
quangnle <mnest2409@yahoo.com>
2017-04-07 18:44:42 +0800

    commit

--- CVGenerator/Models/WorkExperience.cs
+++ CVGenerator/Models/WorkExperience.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CVGenerator.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,26 @@ namespace CVGenerator.Models
 {
     public class WorkExperience
     {
+        private TWorkExperience _entity;
+
+        public int Id { get; set; }
+        public int IdProfile { get; set; }
+        public string Company { get; set; }
+        public string FromTime { get; set; }
+        public string ToTime { get; set; }
+        public string Position { get; set; }
+        public string Description { get; set; }
+
+        public void Bind(TWorkExperience entity)
+        {
+            _entity = entity;
+            Id = _entity.Id;
+            IdProfile = _entity.IdProfile;
+            Company = _entity.Company;
+            FromTime = string.Format("{0}/{1}", _entity.FromMonth, _entity.FromYear);
+            ToTime = string.Format("{0}/{1}", _entity.ToMonth, _entity.ToYear);
+            Position = _entity.Position;
+            Description = _entity.Description;
+        }
     }
 }
