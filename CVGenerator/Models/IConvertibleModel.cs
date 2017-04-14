32f5e5721fae284cbff61fe0f8938a853e807f10
quangnle <mnest2409@yahoo.com>
2017-04-14 17:24:33 +0800

    commit

---
+++ CVGenerator/Models/IConvertibleModel.cs
@@ -0,0 +1,15 @@
+﻿using CVGenerator.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CVGenerator.Models
+{
+    public interface IConvertibleModel<T> where T : IEntity
+    {
+        int Id { get; set; }
+        void Update(T entity);
+        T GetEntity();
+    }
+}
