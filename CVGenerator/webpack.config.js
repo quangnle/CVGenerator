2c37c3163a7812778492e9004e069a4edb928013
Ryan Quoc Le <quoc.letranminh@gmail.com>
2017-05-01 15:57:30 +0800

    commit

---
+++ CVGenerator/webpack.config.js
@@ -0,0 +1,24 @@
+﻿"use strict";
+
+module.exports = {
+    entry: {
+        app: './app/app.module.ts',
+        theme: './Content/Main/js/theme.js'
+    },
+    output: {
+        filename: './dist/[name].bundle.js'
+    },
+    resolve: {
+        extensions: ['.Webpack.js', '.web.js', '.ts', '.js', '.tsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.tsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'ts-loader'
+            }
+        ]
+    },
+    target: "node"
+};
